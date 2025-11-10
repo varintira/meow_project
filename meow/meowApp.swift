c5e166c8ff061b358454fa7f285404604074da0f
varintira <varintira.p@gmail.com>
2025-11-10 13:08:22 +0700

    Initial Commit

---
+++ meow/meowApp.swift
@@ -0,0 +1,17 @@
+//
+//  meowApp.swift
+//  meow
+//
+//  Created by Warintira Pureprasert on 29/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct meowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ meowTests/meowTests.swift
@@ -0,0 +1,17 @@
+//
+//  meowTests.swift
+//  meowTests
+//
+//  Created by Warintira Pureprasert on 29/10/2568 BE.
+//
+
+import Testing
+@testable import meow
+
+struct meowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
